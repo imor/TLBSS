5ac873b7181145c326e894238a74f621a76a8a81
Raminder Singh <richard.the.lionheart@gmail.com>
2012-06-19 00:11:33 +0530

    Added solution to the barrier puzzle
    Also added a template file for new solution to use.

---
+++ chapter3/barrier.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+'''
+Generalize the rendezvous problem to more than two threads.
+Every thread should run the following code:
+
+1 rendezvous
+2 critical point
+'''
+
+from threading_cleanup import *
+threads_waiting_at_rendezvous = 0
+total_no_of_threads = 6
+mutex = Semaphore(1)
+barrier = Semaphore(0)
+
+def func(name):
+    global threads_waiting_at_rendezvous
+    global total_no_of_threads
+    global mutex
+    global barrier
+
+    print name + ' waiting at barrier'
+    mutex.wait()
+    threads_waiting_at_rendezvous += 1
+    if threads_waiting_at_rendezvous == total_no_of_threads:
+        barrier.signal(total_no_of_threads)
+    mutex.signal()
+
+    barrier.wait()
+    print name + ' after barrier'
+
+[Thread(func, str(x)) for x in range(total_no_of_threads)]
